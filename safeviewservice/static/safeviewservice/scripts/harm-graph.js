371c8337980a10cf6d3fbf8a1ed0488435f70184
Montgomery <montgomery.c.anderson@gmail.com>
2016-07-12 13:21:07 +1200

    Added tooltips, highlighting of sunburst arcs (and their path-to-root), added a simple demonstration of searching (case sensitive name search in the sidebar) and lastly a few tweeks in layout values n commenting.

--- safeviewservice/static/safeviewservice/scripts/harm-graph.js
+++ safeviewservice/static/safeviewservice/scripts/harm-graph.js
@@ -8,9 +8,9 @@
  *
  * Created by montgomeryAnderson on 6/06/16.
  *
- * @param d3$svg
- * @param width
- * @param height
+ * @param d3$svg    The d3 selection of the SVG to render the graph within.
+ * @param width     The width of the render.
+ * @param height    The height of the render.
  */
 function HarmGraph(d3$svg, width, height) {
 
@@ -28,6 +28,12 @@ function HarmGraph(d3$svg, width, height) {
 
 
     this.on_tick = function () {
+        var q = d3.geom.quadtree(nodes),
+            i = 0,
+            n = nodes.length;
+
+        while (++i < n) q.visit(Collision(nodes[i]));
+
         // Per tick link updates.
         d3$links
             .attr("x1", function (link) {
@@ -55,7 +61,6 @@ function HarmGraph(d3$svg, width, height) {
             .attr("transform", function (node) {
                 return "translate(" + node.x + "," + node.y + ")";
             });
-        //d3$nodes.each(Collide(0.5));
     };
 
 
@@ -98,27 +103,11 @@ function HarmGraph(d3$svg, width, height) {
 
         force = d3.layout.force()
             .size([width, height])
-            .charge(charge)
-            //.charge(function (node, i) {
-            //    if (node.expanded ) {
-            //        return charge - 300;
-            //    } else {
-            //        return charge;
-            //    }
-            //})
-            .gravity(gravity)
-            .linkDistance(link_distance)
-            //.linkDistance(function (link, i) {
-            //    if (link.source.expanded && link.target.expanded) {
-            //        return link_distance + 100;
-            //    } else if (link.source.expanded || link.target.expanded) {
-            //        return link_distance + 50;
-            //    } else {
-            //        return link_distance;
-            //    }
-            //})
+            .charge(-100)
+            .linkDistance(50)
             .on("tick", this.on_tick);
 
+        // Define the zoom and drag behaviour.
         var zoom = d3.behavior.zoom()
             .on("zoom", this.on_zoom);
         d3$svg
@@ -129,6 +118,11 @@ function HarmGraph(d3$svg, width, height) {
             .on("dblclick.zoom", null);
 
         view = d3$svg.append("g");
+
+        // Define the div for the tooltip.
+        var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
 
         //force.drag()
         //    .on("dragstart", this.on_drag_start);
@@ -157,10 +151,6 @@ function HarmGraph(d3$svg, width, height) {
 
         // Nodes double click.
         d3$nodes.on("dblclick", this.on_dblclick);
-
-        // Nodes update.
-        //d3$nodes.update()
-        //    .each(RenderNode);
 
         // Nodes title.
         d3$nodes.append("title", Title);
@@ -211,35 +201,39 @@ function HarmGraph(d3$svg, width, height) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-function Collision(alpha) {
-    var padding = 1, radius = 12;
-    var quad_tree = d3.geom.quadtree(nodes);
-    return function (node) {
-        var rb = 2 * radius + padding,
-            nx1 = node.x - rb,
-            nx2 = node.x + rb,
-            ny1 = node.y - rb,
-            ny2 = node.y + rb;
-        quad_tree.visit(function (quad, x1, y1, x2, y2) {
-            if (quad.point && (quad.point !== node)) {
-                var x = node.x - quad.point.x,
-                    y = node.y - quad.point.y,
-                    l = Math.sqrt(x * x + y * y);
-                if (l < rb) {
-                    l = (l - rb) / l * alpha;
-                    node.x -= x *= l;
-                    node.y -= y *= l;
-                    quad.point.x += x;
-                    quad.point.y += y;
-                }
+/**
+ * Collision function that takes a node, checks if it collides with others and attempts to resolve
+ * any collisions.
+ * @param node          Node to check for collision.
+ * @returns {Function}  Collision function.
+ */
+function Collision(node) {
+    var r = node.expanded ? 120 : Radius(node) * 2,
+        nx1 = node.x - r,
+        nx2 = node.x + r,
+        ny1 = node.y - r,
+        ny2 = node.y + r;
+    return function (quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+            var x = node.x - quad.point.x,
+                y = node.y - quad.point.y,
+                l = Math.sqrt(x * x + y * y),
+                r = node.radius + quad.point.radius;
+            if (l < r) {
+                l = (l - r) / l * .5;
+                node.x -= x *= l;
+                node.y -= y *= l;
+                quad.point.x += x;
+                quad.point.y += y;
             }
-            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-        });
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
     };
 }
 
 
 /**
+ * Radius function, determines the radius of a node.
  * @return {number}
  */
 function Radius(node) {
@@ -255,6 +249,7 @@ function Radius(node) {
 
 
 /**
+ * Colouring function, determines the colour of a node.
  * @return {string}
  */
 function Color(node) {
@@ -269,6 +264,8 @@ function Color(node) {
 
 
 /**
+ * Title function, titles the nodes - currently doing nothing I believe??
+ * @deprecated
  * @return {string}
  */
 function Title(node) {
@@ -277,6 +274,7 @@ function Title(node) {
 
 
 /**
+ * Text function, determines the name / text of the node that appears (related to a threshold).
  * @return {string}
  */
 function Text(node) {
@@ -289,7 +287,70 @@ function Text(node) {
 
 
 /**
- *
+ * Stashes the original coordinates.
+ * @param data
+ */
+function Stash(data) {
+    data.x0 = data.x;
+    data.dx0 = data.dx;
+}
+
+
+/**
+ * Given a node in a partition layout, return an array of all it's ancestor nodes, highest first,
+ * but excluding the root.
+ * @param node  A node in a partition layout.
+ */
+function GetAncestors(node) {
+    var path = [],
+        current = node;
+    while (current.parent) {
+        path.unshift(current);
+        current = current.parent;
+    }
+    return path;
+}
+
+
+/**
+ * Map the node to a colour, using a attribute selection function.
+ * @param node  The node in question.
+ * @param fn    The attribute selection function, if not specified, compares on an assumed
+ *              "value" attribute.
+ * @returns {*} The colour (as a hex string - using the palette).
+ */
+function MapToPalette(node, fn) {
+    if (fn == null) {
+        fn = function (n) {
+            return n.value;
+        };
+    }
+    // Then..
+    if (fn(node) < 0.15) {
+        return Palette[0];
+    } else if (0.15 < fn(node) < 0.4) {
+        return Palette[1];
+    } else if (0.4 < fn(node) < 0.6) {
+        return Palette[2];
+    } else if (0.6 < fn(node) < 0.85) {
+        return Palette[3];
+    } else { // 0.85 < fn(node)
+        return Palette[4];
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * BELOW ARE NODE RENDERING FUNCTIONS.
+ * These are functions that will take a graph node and render it accordingly to it's expansion or
+ * other future parameters.
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Renders the node, by calling the appropriate more specific rendering function based on expansion
+ * state (e.g. expanded = true, then render a sunburst).
  * @param node
  * @param i
  */
@@ -305,16 +366,31 @@ function RenderNode(node, i) {
 
 
 /**
- *
- * @param d3$node
- * @param node
- * @param i
+ * Render the default representation of a node (a circle) and associate events (on mouse over etc).
+ * @param d3$node   The node selection.
+ * @param node      The node data itself.
+ * @param i         The index.
  */
 function RenderDefault(d3$node, node, i) {
     console.log("Rendering node " + node.id + "[expanded=" + node.expanded + "]");
+
+    var tooltip = d3.select(".tooltip");
     d3$node.append("circle")
         .attr("r", Radius)
-        .style("fill", Color);
+        .style("fill", Color)
+        .on("mouseover", function (node) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0.9);
+            tooltip.html(node.name + "<br>" + node.value)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px")
+        })
+        .on("mouseout", function (node) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
     d3$node.append("text")
         .attr("dx", Radius)
@@ -325,17 +401,48 @@ function RenderDefault(d3$node, node, i) {
 
 /**
  * Takes a d3 node selection (group element) and renders within it a sunburst
- * based on the nodes vulnerability tree.
+ * based on the nodes vulnerability tree and relevant event handlers (e.g. on mouse over).
  * @param d3$node   D3 selection of node element (<g></g>)
  * @param node      The bound node data
  * @param i         The node index
  */
 function RenderSunburst(d3$node, node, i) {
     console.log("Rendering node " + node.id + "[expanded=" + node.expanded + "]");
+
     var height = 60,
         width = 60,
         radius = Math.min(width, height) / 2,
-        color = d3.scale.category20c();
+        color = d3.scale.category20c(),
+        tooltip = d3.select(".tooltip");
+
+    var on_mouseover = function (tree_node) {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", 0.9);
+        tooltip.html(tree_node.name + "<br>" + tree_node.value)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+
+        var sequenceArray = GetAncestors(tree_node);
+        d3$node.selectAll("path")
+            .style("opacity", 0.3);
+
+        d3$node.selectAll("path")
+            .filter(function (tree_node) {
+                if (tree_node === node) return true;
+                return (sequenceArray.indexOf(tree_node) >= 0);
+            })
+            .style("opacity", 1);
+    };
+
+    var on_mouseout = function (node) {
+        tooltip.transition()
+            .duration(500)
+            .style("opacity", 0);
+
+        d3$node.selectAll("path")
+            .style("opacity", 1);
+    };
 
     var partition = d3.layout.partition()
         .sort(null)
@@ -374,7 +481,9 @@ function RenderSunburst(d3$node, node, i) {
                 }
             })
             .style("fill-rule", "evenodd")
-            .each(Stash);
+            .each(Stash)
+        .on("mouseover", on_mouseover)
+        .on("mouseout", on_mouseout);
 
     d3$node.append("text")
         .attr("dx", Radius)
@@ -399,37 +508,11 @@ function RenderSunburst(d3$node, node, i) {
 }
 
 
-function Stash(data) { // stash the
-    data.x0 = data.x;
-    data.dx0 = data.dx;
-}
-
-
-function MapToPalette(node, fn) {
-    if (fn == null) {
-        fn = function (n) {
-            return n.value;
-        };
-    }
-    // Then..
-    if (fn(node) < 0.15) {
-        return Palette[0];
-    } else if (0.15 < fn(node) < 0.4) {
-        return Palette[1];
-    } else if (0.4 < fn(node) < 0.6) {
-        return Palette[2];
-    } else if (0.6 < fn(node) < 0.85) {
-        return Palette[3];
-    } else { // 0.85 < fn(node)
-        return Palette[4];
-    }
-}
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * BELOW IS THE .
+ * BELOW IS THE PALETTE.. yeah, just done as is for now (may make it binding).
  */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 Palette = ["#30638E", "#7EA8BE", "#FBD1A2", "#FC9F5B", "#FF5D73"];
