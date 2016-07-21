d900c77a5b15accca138e31eb50fa832f67dd826
Montgomery <montgomery.c.anderson@gmail.com>
2016-07-22 11:06:17 +1200

    Added a harm xsd under statics/safeviewservice/xml and an empty conversion script * y

---
+++ support/convert.py
@@ -0,0 +1,47 @@
+"""
+A conversion script for converting JSON formatted HARMs to XML variations.
+
+python3 convert.py [-i] <input_file> [<output_file>]
+
+"""
+
+
+import json, xml
+import sys
+
+
+def json_to_xml(json):
+    return
+
+
+def xml_to_json(xml):
+    return
+
+
+def main(argv):
+    if argv[1] is "-i":
+        # argv[2] is input file, optional argv[3] is output file
+
+        pass
+    elif len(argv) > 0:
+        # argv[1] is input file, optional argv[2] is output file
+
+        pass
+    return
+
+
+def prompt_usage():
+    return
+
+
+def run_xml_to_json():
+    return
+
+
+def run_json_to_xml():
+    return
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
