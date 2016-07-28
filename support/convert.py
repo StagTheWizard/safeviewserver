fa95a4094fc0a1496c504f2dbff7bf33e70267de
Montgomery <montgomery.c.anderson@gmail.com>
2016-07-28 15:12:13 +1200

    Three quarters done conversion script - Monty... branch already seriously.

--- support/convert.py
+++ support/convert.py
@@ -33,43 +33,90 @@ def write_xml(harm):
             'xsi:schemaLocation':
                 'http://localhost:8000/safeview/harm http://localhost:8000/static/safevoewservoce/xml/harm.xsd',
         })
-    # <nodes>
-    nodes = etree.Element('nodes')
-    for node in harm['nodes']:
-        # <node name id>
-        xml_node = etree.Element(
-            tag='node',
-            attrib={
-                'id': node.id,
-                'name': node.name
-            })
-        # <values>
-        xml_values = etree.Element(tag='values')
-        for key, value in node['values']:
-            # <value>
-            xml_value = etree.Element(tag=key)
-            xml_value.text = value
-            xml_values.append(xml_value)
-        xml_node.append(xml_values)
-        # <vulnerabilities>
-        xml_vulnerabilities = etree.Element(tag='vulnerabilities')
-        root_vulnerability = node['vulnerabilities'][0]
 
-    xml_harm.append(nodes)
+    # <nodes>
+    xml_nodes = etree.Element(tag='nodes')
+    for node in harm['nodes']:
+        xml_nodes.append(parse_node_to_xml(node))
+    xml_harm.append(xml_nodes)
+
     # <edges>
-    links = etree.Element('edges')
+    xml_edges = etree.Element(tag='edges')
     for edge in harm['edges']:
-        # <edge>
-        continue
-    xml_harm.append(links)
-    #
+        xml_edges.append(parge_edge_to_xml(edge))
+    xml_harm.append(xml_edges)
+
+    # <upperLayers>
+    xml_upper_layers = etree.Element(tag='upperLayers')
+    xml_harm.append(xml_upper_layers)
+
     return xml_harm
 
 
-def parse_vulnerability(vulnerability):
-    # xml_vulnerability = etree.Element(tag=)
-    # return xml_vulnerability
-    pass
+def parse_node_to_xml(node):
+    xml_node = etree.Element(
+        tag='node',
+        attrib={
+            'id': node.id,
+            'name': node.name
+        })
+
+    # <values>
+    xml_values = etree.Element(tag='values')
+    for key, value in node['values']:
+        # <value>
+        xml_value = etree.Element(tag=key)
+        xml_value.text = value
+        xml_values.append(xml_value)
+    xml_node.append(xml_values)
+
+    # <vulnerabilities>
+    xml_vulnerabilities = etree.Element(tag='vulnerabilities')
+    root_vulnerability = node['vulnerabilities'][0]
+    xml_vulnerabilities.append(parse_vulnerability_to_xml(root_vulnerability))
+    xml_node.append(xml_vulnerabilities)
+
+    return xml_node
+
+
+def parge_edge_to_xml(edge):
+    xml_edge = etree.Element(tag='edge')
+
+    xml_source = etree.Element(tag='source')
+    xml_source.text = edge['source']
+    xml_edge.append(xml_source)
+
+    xml_target = etree.Element(tag='target')
+    xml_target.text = edge['target']
+    xml_edge.append(xml_target)
+
+    # <values>
+    xml_values = etree.Element(tag='values')
+    for key, value in edge['values']:
+        # <value>
+        xml_value = etree.Element(tag=key)
+        xml_value.text = value
+        xml_values.append(xml_value)
+
+    xml_edge.append(xml_values)
+
+
+def parse_vulnerability_to_xml(vulnerability):
+    if vulnerability.type is 'vulnerability':
+        # <vulnerability name id>
+        xml_vulnerability = etree.Element(
+            tag=vulnerability.type,
+            attrib={
+                'id': vulnerability.id,
+                'name': vulnerability.name
+            })
+        pass
+    else:  # vulnerability.type is in ['or', 'and']:
+        xml_vulnerability = etree.Element(tag=vulnerability.type)
+        for child in vulnerability.children:
+            # Recurse
+            xml_vulnerability.append(parse_vulnerability_to_xml(child))
+    return xml_vulnerability
 
 
 def json_to_xml(json_file):
@@ -85,29 +132,39 @@ def xml_to_json(xml_file):
 
 
 def main(argv):
-    if argv[1] is "-i":
-        # argv[2] is input file, optional argv[3] is output file
-
-        pass
-    elif len(argv) > 0:
-        # argv[1] is input file, optional argv[2] is output file
-
-        pass
-    return
+    if len(argv) > 0:
+        if argv[0] is "-i":
+            # argv[2] is input file, optional argv[3] is output file
+            run_xml_to_json(argv[1:])
+        # elif len(argv) > 0:
+        else:
+            # argv[1] is input file, optional argv[2] is output file
+            run_json_to_xml(argv)
+    else:
+        prompt_usage()
 
 
 def prompt_usage():
     pass
 
 
-def run_xml_to_json():
-    pass
+def run_xml_to_json(argv):
+    if len(argv) == 0:
+        # prompt
+        return
+    elif len(argv) == 1:
+        pass
+    else:
+        pass
+
+    # validate files
+    # prompt if overwriting
+    # execute
 
 
-def run_json_to_xml():
+def run_json_to_xml(argv):
     pass
 
 
 if __name__ == "__main__":
-    # main(sys.argv[1:])
-    print(write_xml({}))
+    main(sys.argv[1:])
